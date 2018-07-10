035c936027cd9453ed43c1b276dafd2dad68cda0
sramam <sramam@tufan.io>
2018-07-10 05:50:18 -0700

    fix: cleaning up input data, to prevent non-json fields.

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("./filter/index");
 const index_2 = require("./shape/index");
 function dataInvariants(data, filters) {
+    data = JSON.parse(JSON.stringify(data));
     const invariant = index_1.filter(data, filters);
     const shape = index_2.shape(data);
     return { invariant, shape };
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { filter } from './filter/index';
 import { shape as _shape } from './shape/index';
 
 export function dataInvariants(data: any, filters: string[]) {
+  // a simple filters to eliminate non-json fields from data.
+  // this is especially ueful whendata is a js object literal
+  data = JSON.parse(JSON.stringify(data));
   const invariant = filter(data, filters);
   const shape = _shape(data);
   return { invariant, shape};
